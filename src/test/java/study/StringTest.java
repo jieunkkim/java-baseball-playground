1f83f96a4d222b7a663c1bb90a04f7f73c54c243
jieunk.kim <wldmsidqwew@naver.com>
2021-12-28 00:29:33 +0900

    modify test displayName

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -19,14 +19,14 @@ public class StringTest {
      * - "1"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현한다.
      */
     @Test
-    @DisplayName("요구사항 1-1")
+    @DisplayName("split 테스트 (2개 이상 문자)")
     void split_contains() {
         String[] actual = "1,2".split(",");
         assertThat(actual).contains("1", "2");
     }
 
     @Test
-    @DisplayName("요구사항 1-2")
+    @DisplayName("split 테스트 (1개 문자)")
     void split_containsExactly() {
         String[] actual = "1".split(",");
         assertThat(actual).containsExactly("1");
@@ -37,7 +37,7 @@ public class StringTest {
      * - "(1,2)" 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 "1,2"를 반환하도록 구현한다.
      */
     @Test
-    @DisplayName("요구사항 2-1")
+    @DisplayName("substring 테스트")
     void substring() {
         String str = "(1,2)";
         String actual = str.substring(1, str.length() - 1);
@@ -52,7 +52,7 @@ public class StringTest {
      * - JUnit의 @DisplayName을 활용해 테스트 메소드의 의도를 드러낸다.
      */
     @Test
-    @DisplayName("요구사항 3-1")
+    @DisplayName("charAt 테스트")
     void charAt(){
         assertThat("abc".charAt(0)).isEqualTo('a');
         assertThat("abc".charAt(1)).isEqualTo('b');
@@ -60,7 +60,7 @@ public class StringTest {
     }
 
     @Test
-    @DisplayName("요구사항 3-2")
+    @DisplayName("charAt 예외 테스트 (IndexOutOfBounds)")
     void stringIndexOutOfBoundsException(){
         assertThatThrownBy(() -> "abc".charAt(4)).isInstanceOf(StringIndexOutOfBoundsException.class);
     }
