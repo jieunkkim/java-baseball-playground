24c7b190e83960b9ba7037fc71ad7918ee657bc2
jieunk.kim <wldmsidqwew@naver.com>
2021-12-28 00:03:37 +0900

    add testcode (요구사항 1)

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,5 +1,6 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,5 +10,24 @@ public class StringTest {
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    /**
+     * 요구사항 1
+     *  - "1,2"을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현한다.
+     *  - "1"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현한다.
+     */
+    @Test
+    @DisplayName("요구사항 1-1")
+    void split_contains(){
+        String[] actual = "1,2".split(",");
+        assertThat(actual).contains("1", "2");
+    }
+
+    @Test
+    @DisplayName("요구사항 1-2")
+    void split_containsExactly(){
+        String[] actual = "1".split(",");
+        assertThat(actual).containsExactly("1");
     }
 }
